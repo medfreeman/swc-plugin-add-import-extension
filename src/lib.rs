30bfc1384c2d4d676b813b5c5d784d7e11355a5b
Mehdi Lahlou <mlahlou@protonmail.ch>
2022-05-03 22:56:53 +0200

    feat: implement base functionality & tests
    Add `once_cell`, `regex`, `swc_ecma_parser`,
    `swc_ecma_transforms_testing` & `tracing` dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,66 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
 use swc_plugin::{ast::*, plugin_transform, TransformPluginProgramMetadata};
+use tracing::info;
+
+static MJS_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^.*?(?P<ext>\.mjs)?$").unwrap());
 
 pub struct TransformVisitor;
 
 impl VisitMut for TransformVisitor {
-    // Implement necessary visit_mut_* methods for actual custom transform.
-    // A comprehensive list of possible visitor methods can be found here:
-    // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
+    noop_visit_mut_type!();
+
+    fn visit_mut_import_decl(&mut self, imp_declaration: &mut ImportDecl) {
+        imp_declaration.visit_mut_children_with(self);
+
+        info!(
+            "visiting import declaration, source: {}",
+            &imp_declaration.src.value
+        );
+
+        let group = MJS_REGEX.captures(&imp_declaration.src.value);
+        if let Some(group) = group {
+            if group.name("ext") == None {
+                info!("match, source: {:?}", &group[0]);
+                imp_declaration.src =
+                    swc_plugin::ast::Str::from(format!("{}.mjs", &imp_declaration.src.value));
+            }
+        }
+    }
+
+    fn visit_mut_import_specifier(&mut self, imp_specifier: &mut ImportSpecifier) {
+        imp_specifier.visit_mut_children_with(self);
+    }
 }
 
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually via
-/// `__plugin_process_impl(
-///     ast_ptr: *const u8,
-///     ast_ptr_len: i32,
-///     config_str_ptr: *const u8,
-///     config_str_ptr_len: i32,
-///     context_str_ptr: *const u8,
-///     context_str_ptr_len: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */
-///
-/// if plugin need to handle low-level ptr directly. However, there are
-/// important steps manually need to be performed like sending transformed
-/// results back to host. Refer swc_plugin_macro how does it work internally.
 #[plugin_transform]
-pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+pub fn add_mjs_ext_plugin(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
     program.fold_with(&mut as_folder(TransformVisitor))
+}
+
+#[cfg(test)]
+mod transform_visitor_tests {
+    use swc_ecma_transforms_testing::test;
+
+    use super::*;
+
+    fn transform_visitor() -> impl 'static + Fold + VisitMut {
+        as_folder(TransformVisitor)
+    }
+
+    test!(
+        ::swc_ecma_parser::Syntax::default(),
+        |_| transform_visitor(),
+        add_mjs_ext_when_source_is_relative,
+        r#"import { test } from "./entry";"#,
+        r#"import { test } from "./entry.mjs";"#
+    );
+
+    test!(
+        ::swc_ecma_parser::Syntax::default(),
+        |_| transform_visitor(),
+        doesnt_add_mjs_ext_when_source_already_has_it,
+        r#"import { test } from "./entry.mjs";"#,
+        r#"import { test } from "./entry.mjs";"#
+    );
 }
