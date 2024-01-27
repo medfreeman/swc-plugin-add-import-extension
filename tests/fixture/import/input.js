29249cff3dce991fa6dd422365c43061640c2ab6
Mehdi Lahlou <mlahlou@protonmail.ch>
2024-01-27 01:18:32 +0100

    test(plugin): differentiate `import` test

--- tests/fixture/import/input.js
+++ tests/fixture/import/input.js
@@ -1,5 +1,5 @@
-import { test } from "./entry";
-import { test } from "./entry/";
-import { test } from "./entry.js";
-import { test } from "./entry.mjs";
-import { test } from "https://x.nest.land/ramda@0.27.0/source/index.js";
+import test from "./entry";
+import test from "./entry/";
+import test from "./entry.js";
+import test from "./entry.mjs";
+import test from "https://x.nest.land/ramda@0.27.0/source/index.js";
